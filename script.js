157dc9efcb09adb005d97f2112bceabc5b424ccf
gabrielaknupp <127266987+gabrielaknupp@users.noreply.github.com>
2024-09-19 21:29:35 -0300

    atividade

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const perguntas = [
             afirmacao: "afirmação"
            },
            {
-            texto:"Isso é maravilhoso!"
+            texto:"Isso é maravilhoso!",
             afirmacao: "afirmação"
            }
         ]
@@ -22,12 +22,12 @@ const perguntas = [
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+             texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+             afirmacao: "afirmação"
             },
             {
-                texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
+             texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+             afirmacao: "afirmação"
             }
         ]
     },
@@ -39,8 +39,8 @@ const perguntas = [
                 afirmacao: "afirmação"
             },
             {
-                texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
-                afirmacao: "afirmação"
+             texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+             afirmacao: "afirmação"
             }
         ]
     },
@@ -48,12 +48,12 @@ const perguntas = [
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "afirmação"
+             texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
+             afirmacao: "afirmação"
             },
             {
-                texto:"Criar uma imagem utilizando um gerador de imagem de IA."
-                afirmacao: "afirmação"
+             texto:"Criar uma imagem utilizando um gerador de imagem de IA.",
+             afirmacao: "afirmação"
             }
         ]
     },
@@ -61,12 +61,12 @@ const perguntas = [
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
         alternativas: [
             {
-                texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "afirmação"
+             texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+             afirmacao: "afirmação"
             },
             {    
-                texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-               afirmacao: "afirmação"
+             texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+             afirmacao: "afirmação"
             }
         ]    
     },
